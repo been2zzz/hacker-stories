ae18f60734360567258853a8f7379a5c831c71c6
been2zzz <been2zz@naver.com>
2021-09-30 00:43:40 +0900

    props handling, side effect, custom hook

--- src/App.js
+++ src/App.js
@@ -6,6 +6,24 @@ const welcome = {
   title: 'React',
 };
 
+// 리액트 커스텀 훅!!!!
+const useSemiPersistentState = (key, initialState) =>{
+  const [value, setValue] = React.useState(
+    // localStorage.getItem('search') : 저장된 값이 존재하면 초기 상태 설정 사용
+    // 'React' : 기본값
+    localStorage.getItem(key) || initialState
+    );
+    
+    // 첫번째 인수: 사이드 이펙트가 일어나는 함수 => 브라우저 로컬 저장소에 searchTerm 입력
+    // 두번째 인수: 변수의 종속성 '배열'
+    React.useEffect(() => {
+      localStorage.setItem(key, value);
+    }, [value, key]);
+
+    // React.useState에 쓰일 value, setValue 반환
+    return [value, setValue];
+  };
+
 function getTitle(){
   return title;
 }
@@ -28,11 +46,16 @@ const App = () => {
       objectID: 1,
     },
   ];
-  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const [searchTerm, setSearchTerm] = useSemiPersistentState(
+    'search' ,
+    'React'
+  );
   // A
   const handleSearch = event =>{
     // C 상태 변환 함수
     setSearchTerm(event.target.value);
+    localStorage.setItem('search', event.target.value);
   };
 
   const searchedStories = stories.filter(function(story){
@@ -57,28 +80,27 @@ const App = () => {
     );
   };
 
-  const List = props => (
-    <ul>
-    {props.list.map((item) => (
-      <li key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-      </li>
-    ))}
-  </ul>
-);
+  const List = ({ list }) => 
+    list.map(item => <Item key={item.objectID} item={item}/>);
 
-const Search = props => (
+  const Item = ({item}) => (
+    <div>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </div>
+  );
+
+const Search = ({ search, onSearch }) => (
   // 첫번째 : 현재 상태, 두번째: 이 상태를 업데이트하는 함수(상태 업데이트 함수)
   // 배열 구조 분해
     <div> 
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={props.onSearch} value={props.search}/>
-      <p>Search for: <strong>{props.search}</strong></p>
+      <input id="search" type="text" onChange={onSearch} value={search}/>
+      <p>Search for: <strong>{search}</strong></p>
     </div>
 );
 

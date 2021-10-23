b26be65267ef6d687e875c6eff07c09322b1d8ea
devGO <been2zz@naver.com>
2021-10-23 22:49:03 +0900

    정렬

--- src/List.js
+++ src/List.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { sortBy } from 'lodash';
+
 import styled from 'styled-components';
 import { ReactComponent as Check } from './check.svg';
 const StyledItem = styled.div`
@@ -37,17 +39,59 @@ const StyledButton = styled.button`
 const StyledButtonSmall = styled(StyledButton)`
   padding: 5px;
 `;
-const List = React.memo (
-  ({ list, onRemoveItem }) =>
-    console.log('B:List') || 
-    list.map(item => (
-      <Item
-        key={item.objectID} 
-        item={item}
-        onRemoveItem={onRemoveItem}
-      />
-    ))
-);
+
+const SORTS = {
+    NONE: list => list,
+    TITLE: list => sortBy(list, 'title'),
+    AUTHOR: list => sortBy(list, 'author'),
+    COMMENT: list => sortBy(list, 'num_comments').reverse(),
+    POINT: list => sortBy(list, 'points').reverse(),
+};
+
+const List = React.memo(({ list, onRemoveItem }) => {
+    const [sort, setSort] = React.useState('NONE');
+
+    const handleSort = sortKey => {
+        setSort(sortKey);
+    };
+    
+    const sortFunction = SORTS[sort];
+    const sortedList = sortFunction(list);
+    return (
+    <div>
+        <div style={{ display: 'flex' }}>
+        <span style={{ width: '40%' }}>
+          <StyledButtonSmall type="button" onClick={() => handleSort('TITLE')}>
+            Title
+          </StyledButtonSmall>
+        </span>
+        <span style={{ width: '30%' }}>
+          <StyledButtonSmall type="button" onClick={() => handleSort('AUTHOR')}>
+            Author
+          </StyledButtonSmall>
+        </span>
+        <span style={{ width: '10%' }}>
+          <StyledButtonSmall type="button" onClick={() => handleSort('COMMENT')}>
+            Comments
+          </StyledButtonSmall>
+        </span>
+        <span style={{ width: '10%' }}>
+          <StyledButtonSmall type="button" onClick={() => handleSort('POINT')}>
+            Points
+          </StyledButtonSmall>
+        </span>
+        <span style={{ width: '10%' }}>Actions</span>
+      </div>
+        {sortedList.map(item => (
+        <Item
+            key={item.objectID} 
+            item={item}
+            onRemoveItem={onRemoveItem}
+        />
+        ))}
+    </div>
+)});
+
 const Item = React.memo(
   ({ item, onRemoveItem }) => console.log('item')||(
   <StyledItem>

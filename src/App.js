720df06dcc19709227c1cb2e75c4e85bcb4fea3e
devGO <been2zz@naver.com>
2021-10-24 18:03:09 +0900

    More 다음 페이지 불러오기 기능 추가

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ import List from './List';
 
 import styled from 'styled-components';
 const StyledContainer = styled.div`
-  height: 100vw;
   padding: 20px;
-  background: #83a4d4;
-  background: linear-gradient(to left, #b6fbff, #83a4d4);
   color: #171212;
 `;
 
@@ -31,9 +28,43 @@ const StyledButton = styled.button`
     color: #ffffff;
   }
 `;
-const title = 'React';
 
-const initialStories = [
+const API_BASE = 'https://hn.algolia.com/api/v1';
+const API_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+
+const getUrl = (searchTerm, page) => 
+  `${API_BASE}${API_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`;
+
+// url에서 searchTerm만 추출
+const extractSearchTerm = url =>
+  url
+    .substring(url.lastIndexOf('?') + 1, url.lastIndexOf('&')) // query=react
+    .replace(PARAM_SEARCH, ''); // react
+
+const getLastSearches = urls => 
+urls // 빈 배열 result  
+  .reduce((result, url, index) => {
+    const searchTerm = extractSearchTerm(url);
+    
+    if (index === 0) {
+      return result.concat(searchTerm);
+    }
+    
+    const previousSearchTerm = result[result.length -1];
+    
+    if (searchTerm === previousSearchTerm) {
+      return result;
+    } else {
+      return result.concat(searchTerm);
+    }
+    }, [])
+  .slice(-6)
+  .slice(0, -1)
+  .map(extractSearchTerm);
+
+{const initialStories = [
   {
     title: 'React',
     url: 'https://reactjs.org/',
@@ -58,7 +89,7 @@ const getAsyncStories = () =>
       () => resolve({ data: {stories: initialStories } }),
       2000
     )
-  );
+  );}
     // new Promise((resolve, reject) => setTimeout(resolve, 2000));
 
 // 리액트 커스텀 훅!!!!
@@ -76,7 +107,6 @@ const useSemiPersistentState = (key, initialState) =>{
     // 첫번째 인수: 사이드 이펙트가 일어나는 함수 => 브라우저 로컬 저장소에 searchTerm 입력
     // 두번째 인수: 변수의 종속성 '배열'
     React.useEffect(() => {
-      alert('first effect')
       if (!isMounted.current) {
         isMounted.current = true;
       } else {
@@ -103,7 +133,11 @@ const storiesReducer = (state, action) => {
         ...state,
         isLoading: false,
         isError: false,
-        data: action.payload
+        data: 
+          action.payload.page === 0
+            ? action.payload.list
+            : state.data.concat(action.payload.list),
+        page: action.payload.page,
       };
     case 'STORIES_FETCH_FAILURE':
       return {
@@ -111,7 +145,7 @@ const storiesReducer = (state, action) => {
         isLoading: false,
         isError: true,
       };
-    case 'REMOVE_STORIES':
+    case 'REMOVE_STORY':
       return {
         ...state,
         data: state.data.filter(
@@ -129,67 +163,15 @@ const getSumComments = stories => {
     0
   );
 };
-const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
-
-// url에서 searchTerm만 추출
-const extractSearchTerm = url => url.replace(API_ENDPOINT, '');
-
-const getLastSearches = urls => 
-urls // 빈 배열 result  
-  .reduce((result, url, index) => {
-    const searchTerm = extractSearchTerm(url);
-    
-    if (index === 0) {
-      return result.concat(searchTerm);
-    }
-    
-    const previousSearchTerm = result[result.length -1];
-    
-    if (searchTerm === previousSearchTerm) {
-      return result;
-    } else {
-      return result.concat(searchTerm);
-    }
-    }, [])
-  .slice(-6)
-  .slice(0, -1)
-  .map(extractSearchTerm);
-
-const getUrl = searchTerm => `${API_ENDPOINT}${searchTerm}`;
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState(
     'search', 
     'React'
   );
-  // 정적 API URL을 새로운 상태로 설정
-  const [urls, setUrls] = React.useState([getUrl(searchTerm)]);
-
-  const handleSearchInput = event => {
-    setSearchTerm(event.target.value);
-  };
   
-  const handleSearchSubmit = event => {
-    handleSearch(searchTerm);
-    // 브라우저를 다시 로드하는 HTML 폼 기본 동작 막기
-    event.preventDefault();
-  };
-
-  const handleLastSearch = searchTerm => {
-    setSearchTerm(searchTerm); // searchTerm 업데이트
-
-    handleSearch(searchTerm); // urls 배열 concat 
-  };
-
-  const handleSearch = searchTerm =>{
-    const url = getUrl(searchTerm);
-    setUrls(urls.concat(url));
-    // 상태 변환 함수
-    // setSearchTerm(event.target.value);
-    // localStorage.setItem('search', event.target.value);
-  };
-
-  const lastSearches = getLastSearches(urls);
+  // 정적 API URL을 새로운 상태로 설정
+  const [urls, setUrls] = React.useState([getUrl(searchTerm, 0)]);
   
   // story를 비동기적으로 가져오기 위해 초기 상태 빈 배열
   // const [stories, setStories] = React.useState([]);
@@ -197,12 +179,9 @@ const App = () => {
   const [stories, dispatchStories] = React.useReducer(
     storiesReducer,
     // 통합 상태 관리 및 더욱 복잡한 상태 개체를 위해 통합
-    { data: [], isLoading: false, isError: false }
-    );
-    
-  const sumComments = React.useMemo(() => getSumComments(stories), [
-    stories,
-  ]);
+    { data: [], page: 0, isLoading: false, isError: false }
+  );
+  
   const handleFetchStories = React.useCallback(async () => {
     dispatchStories({ type: 'STORIES_FETCH_INIT'});
 
@@ -212,7 +191,10 @@ const App = () => {
       
       dispatchStories({
         type: 'STORIES_FETCH_SUCCESS',
-        payload: result.data.hits,
+        payload:{
+          list: result.data.hits,
+          page: result.data.page,
+        }, 
       });
     } catch {
       dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
@@ -220,11 +202,60 @@ const App = () => {
   }, [urls]);
 
   React.useEffect(() => {
-    alert('second effect')
-
     handleFetchStories();
   }, [handleFetchStories]);
 
+   // useCallback 훅을 사용하여 (종속성 중 하나가 변경된 경우) 리렌더링에만 함수를 생성하도록!
+  const handleRemoveStory = React.useCallback(item => {
+    // 삭제할 아이템을 인수로 하여 필터 조건을 충족하지 않는 모든 아이템 삭제
+    // const newStories = stories.filter(
+    //   story => item.objectID !== story.objectID
+    // );
+    dispatchStories({
+      type: 'REMOVE_STORY',
+      payload: item,
+    });
+    // 살아남은 stories
+    // console.log(newStories)
+    // setStories(newStories);
+  }, []);
+  
+  const handleSearchInput = event => {
+    setSearchTerm(event.target.value);
+  };
+  
+  const handleSearchSubmit = event => {
+    handleSearch(searchTerm, 0);
+    // 브라우저를 다시 로드하는 HTML 폼 기본 동작 막기
+    event.preventDefault();
+  };
+
+  const handleLastSearch = searchTerm => {
+    setSearchTerm(searchTerm); // searchTerm 업데이트
+
+    handleSearch(searchTerm, 0); // urls 배열 concat 
+  };
+
+  const handleMore = () => {
+    const lastUrl = urls[urls.length - 1];
+    const searchTerm = extractSearchTerm(lastUrl);
+    handleSearch(searchTerm, stories.page + 1);
+  };
+
+  const handleSearch = (searchTerm, page) =>{
+    const url = getUrl(searchTerm, page);
+    setUrls(urls.concat(url));
+    // 상태 변환 함수
+    // setSearchTerm(event.target.value);
+    // localStorage.setItem('search', event.target.value);
+  };
+
+  const lastSearches = getLastSearches(urls);
+    
+  const sumComments = React.useMemo(() => getSumComments(stories), [
+    stories,
+  ]);
+  
     // getAsyncStories()
     //   .then(result => {
     //     dispatchStories({
@@ -240,21 +271,6 @@ const App = () => {
     //   );
   // }, [searchTerm]);
 
-  // useCallback 훅을 사용하여 (종속성 중 하나가 변경된 경우) 리렌더링에만 함수를 생성하도록!
-  const handleRemoveStory = React.useCallback(item => {
-    // 삭제할 아이템을 인수로 하여 필터 조건을 충족하지 않는 모든 아이템 삭제
-    // const newStories = stories.filter(
-    //   story => item.objectID !== story.objectID
-    // );
-    dispatchStories({
-      type: 'REMOVE_STORIES',
-      payload: item,
-    });
-    // 살아남은 stories
-    // console.log(newStories)
-    // setStories(newStories);
-  }, []);
-  
   // const searchedStories = stories.data.filter(story =>
   //   // title 중 filter된 story만
   //   stories.title.toLowerCase().includes(searchTerm.toLowerCase())
@@ -274,13 +290,15 @@ const App = () => {
         onLastSearch={handleLastSearch}
       />
       <hr/>
-
+      <List 
+          list={stories.data} 
+          onRemoveItem={handleRemoveStory} />
       {stories.isLoading ? (
         <p>Loding...</p>
         ) : (
-          <List 
-          list={stories.data} 
-          onRemoveItem={handleRemoveStory} />
+          <StyledButton type="button" onClick={handleMore}>
+            More
+          </StyledButton>
           )}
       {/* 조건이 참이면 && 뒷부분이 출력됨 거짓일 시 무시*/}
       {stories.isError && <p>Something went Wrong ...</p>}
@@ -302,4 +320,5 @@ const LastSearches = ({ lastSearches, onLastSearch }) => (
   ))}
   </>
 );
+
 export default App;
